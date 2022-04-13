43041936cac43dd9e876beb1f1d8ebf750c7d69c
xChaoticVoid <95597125+xChaoticVoid@users.noreply.github.com>
2022-04-13 18:30:07 -0400

    Add files via upload

---
+++ Tower of Jump.lua
@@ -0,0 +1,105 @@
+loadstring(game:HttpGet("https://pastebin.com/raw/E6eyfLrA"))()
+
+local Mercury = loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
+local GUI = Mercury:Create{
+    Name = "ChaosHub",
+    Size = UDim2.fromOffset(600, 400),
+    Theme = Mercury.Themes.Dark,
+    Link = "https://github.com/deeeity/mercury-lib"
+}
+
+GUI:Notification{
+	Title = "Game Detected",
+	Text = "Tower of Jump",
+	Duration = 3,
+	Callback = function() end
+}
+
+GUI:Notification{
+	Title = "Discord",
+	Text = "Make Sure To Join The Discord Server! :D",
+	Duration = 3,
+	Callback = function() end
+}
+
+GUI:Credit{
+	Name = "Chaotic_Void",
+	Description = "Hub Owner",
+}
+
+local Tab = GUI:Tab{
+	Name = "Main",
+	Icon = "rbxthumb://type=Asset&id=3517717568&w=150&h=150"
+}
+
+Tab:Button{
+	Name = "Auto Win",
+	Description = nil,
+	Callback = function() 
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Storage.End.WinBrick.CFrame
+    end
+}
+
+Tab:Button{
+	Name = "Server Hop",
+	Description = nil,
+	Callback = function() 
+        Teleport()
+    end
+}
+
+local Tab = GUI:Tab{
+	Name = "Misc",
+	Icon = "rbxthumb://type=Asset&id=3331928717&w=150&h=150"
+}
+
+Tab:Textbox{
+	Name = "Walk Speed",
+	Callback = function(text) 
+        local wad = text;
+        local Humanoid = game:GetService("Players").LocalPlayer.Character.Humanoid;
+        Humanoid:GetPropertyChangedSignal("WalkSpeed"):Connect(function()
+        Humanoid.WalkSpeed = wad;
+        end)
+        Humanoid.WalkSpeed = wad;
+    end
+}
+
+Tab:Textbox{
+	Name = "Gravity",
+	Callback = function(text) 
+        workspace.Gravity = text
+    end
+}
+
+Tab:Button{
+	Name = "Inf Jump",
+	Description = nil,
+	Callback = function() 
+    local infjump = true
+    game:GetService("UserInputService").JumpRequest:connect(function()
+    if infjump then
+    game.Players.LocalPlayer.Character:FindFirstChildOfClass("Humanoid"):ChangeState("Jumping")
+   end
+end)
+end
+}
+
+local Tab = GUI:Tab{
+	Name = "Discord",
+	Icon = "rbxthumb://type=Asset&id=9334666836&w=150&h=150"
+}
+
+Tab:Button{
+	Name = "Copy Discord Server Invite",
+	Description = nil,
+	Callback = function()
+        GUI:Notification{
+            Title = "Discord",
+            Text = "Discord Invite Has Been Copied!",
+            Duration = 3,
+            Callback = function() end
+        }
+        setclipboard("https://discord.gg/wk7Wer9rGk")
+    end
+}
