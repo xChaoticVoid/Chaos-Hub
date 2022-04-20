c5b353bbc04c9546d8c3e7f0f7a06a44e1d923e3
xChaoticVoid <95597125+xChaoticVoid@users.noreply.github.com>
2022-04-20 14:48:10 -0400

    Add files via upload

---
+++ Stand Upright.lua
@@ -0,0 +1,189 @@
+local ESP = loadstring(game:HttpGet("https://kiriot22.com/releases/ESP.lua"))()
+
+local Mercury = loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
+local GUI = Mercury:Create{
+    Name = "Chaos",
+    Size = UDim2.fromOffset(600, 400),
+    Theme = Mercury.Themes.Dark,
+    Link = "https://github.com/deeeity/mercury-lib"
+}
+
+GUI:Notification{
+	Title = "Game Detected",
+	Text = "Stand Upright: Rebooted",
+	Duration = 3,
+	Callback = function() end
+}
+
+GUI:Notification{
+	Title = "Discord",
+	Text = "Make Sure To Join The Discord Server! :D",
+	Duration = 3,
+	Callback = function() end
+}
+
+GUI:Credit{
+	Name = "Chaotic_Void",
+	Description = "Hub Owner",
+}
+
+GUI:Credit{
+	Name = "sannin",
+	Description = "Anti-Cheat Bypass",
+}
+
+local Tab = GUI:Tab{
+	Name = "Main",
+	Icon = "rbxthumb://type=Asset&id=3517717568&w=150&h=150"
+}
+
+Tab:Button{
+	Name = "Bypass Anti-Cheat",
+	Description = nil,
+	Callback = function()
+		loadstring(game:HttpGet'https://github.com/sannin9000/scripts/raw/main/Stand%20Upright%20Bypass.lua')()
+    end
+}
+
+Tab:Button{
+	Name = "Grab Items",
+	Description = nil,
+	Callback = function()
+			get()
+    end
+}
+
+local Tab = GUI:Tab{
+	Name = "Visuals",
+	Icon = "rbxthumb://type=Asset&id=5219208999&w=150&h=150"
+}
+
+Tab:Toggle{
+	Name = "Enable Esp",
+	StartingState = false,
+	Description = nil,
+	Callback = function(state) 
+        ESP:Toggle(state)
+    end
+}
+
+Tab:Toggle{
+	Name = "Enable Players",
+	StartingState = false,
+	Description = nil,
+	Callback = function(state) 
+        ESP.Players = state
+    end
+}
+
+Tab:Toggle{
+	Name = "Enable Boxes",
+	StartingState = false,
+	Description = nil,
+	Callback = function(state) 
+        ESP.Boxes = state
+    end
+}
+
+Tab:Toggle{
+	Name = "Enable Names",
+	StartingState = false,
+	Description = nil,
+	Callback = function(state) 
+        ESP.Names = state
+    end
+}
+
+Tab:Toggle{
+	Name = "Enable Tracers",
+	StartingState = false,
+	Description = nil,
+	Callback = function(state) 
+        ESP.Tracers = state
+    end
+}
+
+Tab:Toggle{
+	Name = "Item Esp",
+	StartingState = false,
+	Description = nil,
+	Callback = function(state) 
+        ESP.item = state
+    end
+}
+
+local Tab = GUI:Tab{
+	Name = "Misc",
+	Icon = "rbxthumb://type=Asset&id=3331928717&w=150&h=150"
+}
+
+Tab:Slider{
+	Name = "Gravity",
+	Default = 50,
+	Min = 0,
+	Max = 200,
+	Callback = function(text) 
+            workspace.Gravity = text
+    end
+}
+
+Tab:Button{
+	Name = "Inf Jump",
+	Description = nil,
+	Callback = function() 
+    local infjump = true
+    game:GetService("UserInputService").JumpRequest:connect(function()
+    if infjump then
+    game.Players.LocalPlayer.Character:FindFirstChildOfClass("Humanoid"):ChangeState("Jumping")
+   end
+end)
+end
+}
+
+local Tab = GUI:Tab{
+	Name = "Discord",
+	Icon = "rbxthumb://type=Asset&id=9334666836&w=150&h=150"
+}
+
+Tab:Button{
+	Name = "Copy Discord Server Invite",
+	Description = nil,
+	Callback = function()
+        GUI:Notification{
+            Title = "Discord",
+            Text = "Discord Invite Has Been Copied!",
+            Duration = 3,
+            Callback = function() end
+        }
+        setclipboard("https://discord.gg/wk7Wer9rGk")
+    end
+}
+
+
+ESP:AddObjectListener(game:GetService("Workspace").Items, {
+    Color =  Color3.new(1,1,0),
+    Type = "Tool",
+    PrimaryPart = function(obj)
+        local hrp = obj:FindFirstChild("Handle")
+        while not hrp do
+            wait()
+            hrp = obj:FindFirstChild("Handle")
+        end
+        return hrp
+    end,
+    Validator = function(obj)
+        return not game.Players:GetPlayerFromCharacter(obj)
+    end,
+    IsEnabled = "item",
+})
+
+function get()
+	while getgenv().get == true do
+		for i, v in pairs(game:GetService("Workspace").Items:GetDescendants()) do
+			if v.Name == "TouchInterest" and v.Parent then
+				firetouchinterest(game:GetService('Players').LocalPlayer.Character.HumanoidRootPart, v.Parent, 0)
+			end
+		end
+	end
+	wait()
+end
